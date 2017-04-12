37fa455b7d89d0715f5d8665ecf58302512d2cbe
failathon <luke@blackduck.nu>
2017-04-12 14:05:55 -0400

    OSX POSH support, more error checking & reporting
    $IsWindows & $IsOSX POSH based OS detection to define different paths
    Additional quotation for paths involving spaces (was triggering faults on POSH OSX)
    Added check to not attempt to move pictures that don't conform to naming convention
    Added additional logging to console

--- SortDropboxCameraUploads.ps1
+++ SortDropboxCameraUploads.ps1
@@ -1,36 +1,87 @@
-#
 # Many thanks to /u/sysmadmin for this
-#
+# Added OSX support
+
+$DryRun = $False
+
 #Creating the structure within the Dropbox folder, following the Dropbox\Photos\Year\Month\Images method of sorting. The root of the Dropbox directory must be set, e.g. I:\Dropbox
-$DropboxRoot = "E:\Dropbox"
+If ($IsWindows -eq $True) {
+    $DropboxRoot = "E:\Dropbox"
+    $DropboxCameraPath = $DropboxRoot + "\Camera Uploads\"
+    $DropboxPhotos = $DropboxRoot + "\Photos\"
+} elseif ($IsOSX -eq $True) {
+    $DropboxRoot = "/Users/sky/Dropbox"
+    $DropboxCameraPath = $DropboxRoot + "/Camera Uploads/"
+    $DropboxPhotos = $DropboxRoot + "/Photos/"
+} else {
+    Write-Host "ERROR: Platform not Windows or OSX!"
+    Exit
+}
 
 #Sorts all of the images in Camera Uploads, and sets the earliest year as the "FirstYear" to start creating, then checks the current year to know when to create the new years structure
-$FirstPhoto = ((Get-ChildItem -Path "$DropboxRoot\Camera Uploads" | Sort-Object -Property Name) | Select-Object -First 1)
+$FirstPhoto = ((Get-ChildItem -Path "$DropboxCameraPath" | Sort-Object -Property Name) | Select-Object -First 1)
 $FirstYear = ($FirstPhoto.Name[0] + $FirstPhoto.Name[1] + $FirstPhoto.Name[2] + $FirstPhoto.Name[3])
 $CurrentYear = (Get-Date -UFormat %Y)
 
 #Verifying the Photos directory exists, and creates it if not. This only matters the first time the script is run
-If ((Test-Path "$DropboxRoot\Photos") -ne $True){New-Item -Path "$DropboxRoot\" -Name Photos -ItemType Directory}
-
+If ((Test-Path $DropboxPhotos) -ne $True) {
+    New-Item -Path "$DropboxRoot" -Name "Photos" -ItemType Directory
+}
 
 $FirstYear..$CurrentYear | Foreach {
-    If ((Test-Path "$DropboxRoot\Photos\$_") -ne $True)
+    $YearPath = $DropboxPhotos + $_
+    # Check if path exists, if not, let's create it
+    If ((Test-Path $YearPath) -ne $True)
     {
-        New-Item -Path "$DropboxRoot\Photos\" -Name $_ -ItemType Directory
-        $i = 1
-        while ($i -lt 13)
-        {New-Item -ItemType Directory -Name ((Get-Date -UFormat %m -Month $i) + " - " + (Get-Date -UFormat %B -Month $i)) -Path "$DropboxRoot\Photos\$_"; $i++}
+        Write-Host "[*] Creating directory" $_
+        If($DryRun) {
+            Write-Host "DRYRUN: would have created $YearPath"
+        } else {
+            New-Item -Path "$DropboxPhotos" -Name $_ -ItemType Directory
+            $i = 1
+            while ($i -lt 13) {
+                New-Item -ItemType Directory -Name ((Get-Date -UFormat %m -Month $i) + " - " + (Get-Date -UFormat %B -Month $i)) -Path "$YearPath"; $i++
+            }
+        }
     }
 }
 
 #Getting all images in the Camera Uploads folder (limit this with (Get-ChildItem * -Include 2014-08-01*) for example, when testing); Excluding .* to skip the Dropbox files
-$Pictures = (Get-ChildItem -Path "$DropboxRoot\Camera Uploads" -Exclude .*)
+$Pictures = (Get-ChildItem -Path "$DropboxCameraPath" -Exclude .*)
+Write-Host "[*]" $Pictures.length.tostring() "pictures found in $DropboxCameraPath"
 
 # Loop through all images, move to folder containing the year (indices 0-3), and then the folder containing the month (indices 5-6*) -asterisk required on Move-Item , as the folder names contain the numeric representation as well as the full month name
 Foreach ($Pic in $Pictures){
-    $YearDirectory = ($Pic.Name[0] + $Pic.Name[1] + $Pic.Name[2] + $Pic.Name[3])
-    $MonthDirectory = ($Pic.Name[5] + $Pic.Name[6])
-    Move-Item $Pic -Destination "$DropboxRoot\Photos\$YearDirectory\$MonthDirectory*"
-    # Verify variables with below:
-    #$Destination + "\" + $MonthDirectory
+    $temp = $Pic.Name.Split('-')
+    $YearDirectory = $temp[0]
+    $MonthDirectory = $temp[1]
+
+    if($MonthDirectory.length -lt 2 -Or $MonthDirectory.length -gt 2) {
+        Write-Host "ERROR: Can't process $Pic"
+    } else {
+        $MonthDirectory = ((Get-Date -UFormat %m -Month $MonthDirectory) + " - " + (Get-Date -UFormat %B -Month $MonthDirectory))
+        Write-Host "[*] Moving $pic into $DropboxPhotos -> $YearDirectory -> $MonthDirectory"
+
+        if($DryRun) {
+            Write-Host "DRYRUN: Would have moved $pic into $DropboxPhotos$YearDirectory/$MonthDirectory/"
+        } else {
+            Try {
+                If ($IsWindows -eq $True) {
+                    Move-Item "$Pic" -Destination "$DropboxPhotos$YearDirectory\$MonthDirectory/"
+                } elseif ($IsOSX -eq $True) {
+                    Move-Item "$Pic" -Destination "$DropboxPhotos$YearDirectory/$MonthDirectory/"
+                } else {
+                    Write-Host "ERROR: Platform not Windows or OSX! (Should not fail here either!)"
+                    Exit
+                }
+                # Verify variables with below:
+                #$Destination + "\" + $MonthDirectory
+            }
+            Catch {
+                Write-Host "ERROR moving file"
+                Write-Host $_.Exception.Message
+                Write-Host $_.Exception.ItemName
+                Exit
+            }
+        }
+    }
 }
