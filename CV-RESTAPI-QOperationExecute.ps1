52b40ede063ae3de1607efb38ff45296985dbe5e
Skywalker <luke@blackduck.nu>
2018-02-23 13:36:42 -0500

    Create CV-RESTAPI-QOperationExecute.ps1

---
+++ CV-RESTAPI-QOperationExecute.ps1
@@ -0,0 +1,40 @@
+$WebServer = "http://whats-your-webserver-name:81/SearchSvc/CVWebService.svc"
+$username = "setuserhere"
+$password = "setpasshere"
+
+$SCPClientArray = @()
+$SubClientPolicyName = "Laptop plan Windows subclient policy"
+
+$headers = @{}
+$headers["Accept"] = "application/json"
+
+$Body = @{ 
+    username = $username
+    password = [Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($password))
+}
+
+# Login to Commvault REST API
+$Result = Invoke-RestMethod -Method POST -Uri "$WebServer/Login" -Headers $headers -ContentType "application/json" -Body $($Body | ConvertTo-Json)
+
+# Save QSDK token
+$headers["Authtoken"] = $Result.token
+
+# Override accept header - qcommand only works with XML
+# Set qoperation execscript command
+# And do the work
+$headers["Accept"] = "application/xml"
+$Body = "qoperation execscript -sn GetSubclientPolicy.sql -si scp=$SubClientPolicyName"
+$Result = Invoke-RestMethod -Method POST -Uri "$WebServer/QCommand" -Headers $headers -Body $Body
+
+If ($Result.CVGui_GenericResp.errorCode) { 
+    # Error handling
+    Write-Host "something went wrong, do something"
+} else {
+    $ResponseXML.ExecScriptOutput.FieldValue.Clientname | ForEach-Object {
+        If($_ -notlike "*GetSubclientPolicycompleted*") {
+            $SCPClientArray += $_ 
+        }
+    }
+
+    Write-Host $SCPClientArray
+}
