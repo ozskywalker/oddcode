48adeb34c5c71445c608ef5705e9427f9daef02c
Luke Walker <luke@lwalker.me>
2020-08-25 00:53:13 -0400

    Fixed docker login hang issue

--- docker_update.ps1
+++ docker_update.ps1
@@ -1,4 +1,10 @@
-(docker login) -and (docker images --format '{{.Repository}}' |% { if ($_) { docker pull $_ }})
+if (docker login) {
+    docker images --format '{{.Repository}}' |% { if ($_) { docker pull $_ }}
 
-$dangling = $(docker images --filter "dangling=true" -q --no-trunc)
-if ($dangling.Count -gt 0) { docker rmi $dangling }
+    $dangling = $(docker images --filter "dangling=true" -q --no-trunc)
+    if ($dangling.Count -gt 0) { docker rmi $dangling } else {
+        Write-Host "no cleanup required"
+    }
+} else {
+    Write-Host "login issues"
+}
