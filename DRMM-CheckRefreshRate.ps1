89cc7a99ec3802bcb12ef6a79d488b5109eb4cb1
Luke Walker <luke@lwalker.me>
2021-11-26 16:32:35 -0500

    DRMM-CheckRefreshRate: Added filter for Hyper-V & RDP display adapters

--- DRMM-CheckRefreshRate.ps1
+++ DRMM-CheckRefreshRate.ps1
@@ -1,15 +1,21 @@
-<# Datto RMM Monitor component :: check monitor refresh rate :: build 1
+<# Datto RMM Monitor component :: check monitor refresh rate :: build 2
 
    checks that the refresh rate across all screens doesn't fall below a minimum value
    default: 59 (=59.00 Hz)
+
+   changelog:
+    build 2 -> filters out Hyper-V & RDP display adapters 
 #>
 
 $desiredRefreshRate = 59;
 $lowestRate = 999;
+$DriverFilterList = @("wvmbusvideo.inf", "rdpidd.inf")
 
 Get-WmiObject Win32_VideoController | ForEach-Object {
-    if (($_.CurrentRefreshRate -is [uint32]) -and ($_.CurrentRefreshRate -lt $lowestRate)) {
-        $lowestRate = $_.CurrentRefreshRate
+    if (!$DriverFilterList.Contains($_.InfFilename)) {
+        if (($_.CurrentRefreshRate -is [uint32]) -and ($_.CurrentRefreshRate -lt $lowestRate)) {
+            $lowestRate = $_.CurrentRefreshRate
+        }
     }
 }
 
@@ -24,7 +30,13 @@ if ($lowestRate -lt $desiredRefreshRate) {
     exit 1
 } else {
     Write-Host "<-Start Result->"
-    Write-Host "min=$lowestRate"
+    
+    if ($lowestRate -eq 999) {
+        Write-Host "No physical adapters or displays detected.  All OK."
+    } else {
+        Write-Host "min=$lowestRate"
+    }
+
     Write-Host "<-End Result->"
     exit 0
 }
