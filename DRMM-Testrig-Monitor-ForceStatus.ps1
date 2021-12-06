46c74e22fe75c3963d0cfdd235efed455bc06a02
Luke Walker <luke@lwalker.me>
2021-12-06 12:14:58 -0500

    DRMM-Testrig-Monitor-ForceStatus: added default vars to comments

--- DRMM-Testrig-Monitor-ForceStatus.ps1
+++ DRMM-Testrig-Monitor-ForceStatus.ps1
@@ -1,6 +1,20 @@
 # Script monitor for Datto RMM (DRMM) that lets you force any status or message
 # Useful to test alerts, response components and other activities related to monitoring DRMM endpoints
 
+# default variables to use
+
+# Invoking an alert / response component
+# resultMessage (String) = Not Working
+# resultStatus (String) = ALERT
+# diagMessage (String) = I am a diagnostic message
+# exitCodeToUse (String) = 1
+
+# No alert / success
+# resultMessage (String) = All good
+# resultStatus (String) = ALERT
+# diagMessage (String) = I am a diagnostic message
+# exitCodeToUse (String) = 0
+
 $resultMessage = $env:ResultMessage
 $resultStatus = $env:ResultStatus
 $diag = $env:diagMessage
